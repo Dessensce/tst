e05eac45d5ec7789580eb6c8dda8370566d4eb6b
Dessensce <117656317+Dessensce@users.noreply.github.com>
2026-01-06 21:09:24 +0800

    Implement continuous logging of 'stuff' variable
    Added a loop to continuously print the 'stuff' variable.

--- script.lua
+++ script.lua
@@ -1,11 +1,14 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 local ReplicatedStorage_upvr = game:GetService("ReplicatedStorage")
 
+local stuff = "stuff"
+
 for _, remotes in ReplicatedStorage_upvr:GetDescendants() do
 	if remotes:IsA("RemoteEvent") then
 		if #remotes.Name == 36 then
 			remotes.OnClientEvent:Connect(function(mode, ...)
 				if mode == "lwchzwil" then
+					stuff = ...
 					print(...)
 					print("i should have logged it")
 				end
@@ -14,4 +17,6 @@ for _, remotes in ReplicatedStorage_upvr:GetDescendants() do
 	end
 end
 
-
+while task.wait() do
+	print(stuff)
+end
